6f126ef05aaaff1e72d49b6e33776efa97dfc492
radup99 <raduplacinta85@gmail.com>
2024-09-01 12:24:13 +0300

    Add IRepository class

---
+++ OnlineTechShopApi/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineTechShopApi.Repositories
+{
+    public interface IRepository<T>
+    {
+        public Task Create(T t);
+        public Task<T> ReadById(int id);
+        public Task Update(int id, T t);
+        public Task DeleteById(int id);
+    }
+}
