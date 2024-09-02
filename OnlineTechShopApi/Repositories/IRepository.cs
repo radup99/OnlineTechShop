d83da0aef3b63f44098491dbc89fcec601792f3e
radup99 <raduplacinta85@gmail.com>
2024-09-02 23:17:18 +0300

    Update ReadById in IRepository

--- OnlineTechShopApi/Repositories/IRepository.cs
+++ OnlineTechShopApi/Repositories/IRepository.cs
@@ -3,7 +3,7 @@
     public interface IRepository<T>
     {
         public Task Create(T t);
-        public Task<T> ReadById(int id);
+        public Task<T?> ReadById(int id);
         public Task Update(int id, T t);
         public Task DeleteById(int id);
     }
