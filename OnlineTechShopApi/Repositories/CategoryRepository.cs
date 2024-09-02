dae3d485ab81fccaf5387ac97a6d7a3f02c8f948
radup99 <raduplacinta85@gmail.com>
2024-09-02 23:38:50 +0300

    Add CategoryRepository

---
+++ OnlineTechShopApi/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineTechShopApi.Database;
+using OnlineTechShopApi.Entities;
+
+namespace OnlineTechShopApi.Repositories
+{
+    public class CategoryRepository(MainDbContext dbContext) : IRepository<Category>
+    {
+        private readonly MainDbContext _dbContext = dbContext;
+        private readonly DbSet<Category> _dbSet = dbContext.Set<Category>();
+
+        public async Task Create(Category category)
+        {
+            await _dbSet.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Category?> ReadById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task Update(int id, Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteById(int id)
+        {
+            await _dbSet.Where(p => p.Id == id).ExecuteDeleteAsync();
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
